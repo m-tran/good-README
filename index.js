adcb15df195e3342628098d6ef3048b910191c02
Michelle Tran <mptran0101@berkeley.edu>
2020-05-30 00:46:36 -0600

    added badge creator

--- index.js
+++ index.js
@@ -19,9 +19,44 @@ async function generateREADME() {
         const profileImg = data.avatar_url;
 
         console.log(email, profileImg);
+        
+        // project title
+        const { projectTitle } = await inquirer.prompt({
+            type: "input",
+            message: "What is the title of your project?",
+            name: "projectTitle",
+        })
+
+        // project description 
+
+        // get badge
+        const { userlabel } = await inquirer.prompt({
+            type: "input",
+            message: "What label do you want your badge to have?",
+            name: "userlabel",
+        });
+
+        const { usermessage } = await inquirer.prompt({
+            type: "input",
+            message: "What message do you want your badge to have?",
+            name: "usermessage",
+        });
+
+        const { usercolor } = await inquirer.prompt({
+            type: "list",
+            message: "What color do you want your badge to have?",
+            name: "usercolor",
+            choices: ["brightgreen", "green", "yellowgreen", "yellow", "orange", "red", "blue",
+                "lightgrey", "success", "important", "critical", "informational", "inactive", "blueviolet",
+                "ff69b4", "9cf"],
+        });
+
+        const badgeURL = `https://img.shields.io/badge/${userlabel}-${usermessage}-${usercolor}`;
+
+        console.log(badgeURL);
 
     } catch (err) {
-        console.log({error: (err)});
+        console.log({ error: (err) });
     }
 }
 
