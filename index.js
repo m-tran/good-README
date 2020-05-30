0417a5c78ca1db57eb104bc48481768541a0a4aa
Michelle Tran <mptran0101@berkeley.edu>
2020-05-29 22:33:15 -0600

    wrap in async fxn

--- index.js
+++ index.js
@@ -2,20 +2,26 @@ const fs = require("fs");
 const axios = require("axios");
 const inquirer = require("inquirer");
 
-inquirer
-    .prompt({
-        message: "Enter your GitHub username",
-        name: "username",
-    })
-    .then(function ({ username }) {
-        const queryURL = `https://api.github.com/users/${username}`;
+generateREADME();
 
-        axios.get(queryURL).then((res) => {
-            // console.log(res.data);
-            const email = res.data.email;
-            const profileImg = res.data.avatar_url;
-
-            console.log(profileImg);
-            console.log(email);
+async function generateREADME() {
+    try {
+        const { username } = await inquirer.prompt({
+            message: "Enter your GitHub username",
+            name: "username",
         });
-    });
+
+        const { data } = await axios.get(
+            `https://api.github.com/users/${username}`
+        );
+
+        const email = data.email;
+        const profileImg = data.avatar_url;
+
+        console.log(email, profileImg);
+
+    } catch (err) {
+        console.log({error: (err)});
+    }
+}
+
